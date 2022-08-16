a72cbe6628274b6671e506b1073a1c0e0b868f59
Daria Approvato <dariaapprovato@gmail.com>
2022-08-16 19:17:14 -0400

    commit tree f5c69ace72cb245bb4242bd55623ea3d46db9a6f

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+function submitData(name, email) {
+    const formData = {
+        name: name,
+        email: email
+      }
+      
+      const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify(formData)
+      }
+      
+      return fetch("http://localhost:3000/users", configurationObject)
+        .then(function (response) {
+          return response.json()
+        })
+        .then(function (data) {
+           console.log("print;", data)
+           const newDiv = document.createElement("div")
+           newDiv.innerHTML = data.id
+           document.body.appendChild(newDiv)
+        })
+        .catch(function (error) {
+          alert("Failed to post!")
+          console.log(error.message)
+          const newDivTwo = document.createElement("div")
+          newDivTwo.innerHTML = error
+          document.body.appendChild(newDivTwo)
+        })
+}
